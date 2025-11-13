2bb32393513d96a8f6d173347ef00c51c1e73635
nidhi tiwari <nidhitiwari2807@gmail.com>
2025-11-13 21:03:40 +0530

    .h

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-
+import React from "react";
 import Categories from "../components/Categories";
 import Trending from "../components/Trending";
 import HeroSection from "../components/HeroSection";
