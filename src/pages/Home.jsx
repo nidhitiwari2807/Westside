6651f116993ac05c6817070709ee68a757db13ae
nidhi tiwari <nidhitiwari2807@gmail.com>
2025-11-13 21:25:51 +0530

    done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Categories from "../components/Categories";
 import Trending from "../components/Trending";
-import HeroSection from "../components/HeroSection";
+import HeroSection from "../Components/Categories";
 
 export default function Home() {
   return (
